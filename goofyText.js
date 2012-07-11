4ad4366816ba73809a5f79e6266b09c22fc57c67
Andy Ortlieb <andy.ortlieb@corvisa.com>
2012-07-11 12:55:50 -0500

    Frameworking the production version

---
+++ goofyText.js
@@ -0,0 +1,131 @@
+;(function(){
+
+	// Public declarations
+
+	/** 
+	 * public goofyText
+	 * Namespace for all public goofyText references and methods
+	 */
+	goofyText = {};
+
+	// End public declarations
+
+	
+	// Private declarations
+	var 
+		/**
+		 * private cursor
+		 * maintains a reference to the single global cursor.
+		 */
+		cursor
+		,cursorCycleTimeout
+		,cursorCycleNextColor = 'black'
+		,cursorHistory = []
+
+	; // End private declarations
+
+
+	/**
+	 * private method on
+	 * enables events, intended for DOM objects.	 
+	 */
+	function on(evtName, callBack){
+		if (this.addEventListener){
+			return this.addEventListener(evtName, callBack)
+
+		} else if (this.attachEvent){
+			return this.attachEvent(evtName, callBack)
+
+		} else {
+			throw "Unable to attach event."
+		}
+	}
+
+	/**
+	 * private method off
+	 * disables events, intended for DOM objects.
+	 */
+	function off(evtName, callBack){
+		if (this.removeEventListener){
+			return this.removeEventListener(evtName, callBack)
+
+		} else if (this.detachEvent){
+			return this.detachEvent(evtName, callBack)
+
+		} else {
+			throw "Unable to attach event."
+		}
+	}
+
+	/**
+	 * private cursorHistoryClear
+	 */
+	function cursorHistoryClear(){
+		for (var i=0;i<cursorHistory.length; ++i){
+			if (cursorHistory[i] === cursor) continue;
+			cursorHistory[i].style.borderLeft = 'inherit';
+			cursorHistory[i].style.borderLeft = 'inherit';
+		}
+	}
+
+	/**
+	 * private cursorCycle
+	 * cycles the cursor, wherever it may be
+	 */
+	function cursorCycle(update){
+		console.log('cursorCycle');
+		cursorHistoryClear();
+
+		// Set the cursor color
+		if (cursor && cursor.style){
+
+			if (update){
+				cursorCycleNextColor = 'black';
+			} else {
+				cursorCycleNextColor= cursorCycleNextColor==='white'?'black':'white';
+			}
+
+			cursor.style.borderLeft =  '1px solid '+cursorCycleNextColor;
+			cursor.style.borderLeft = '-1px';
+		}
+
+		clearTimeout(cursorCycleTimeout);
+
+		// Loop forever
+		if (update){
+			cursorCycleTimeout = setTimeout( cursorCycle, 960 )
+
+		} else {
+			cursorCycleTimeout = setTimeout( cursorCycle, 480 )
+
+		}
+
+
+	}
+
+
+	/****************************************
+	 ****************************************
+
+				One-time setup
+
+	 ****************************************
+	 ****************************************/
+
+	 // DEBUG STUFF
+	 if (true){
+	 	goofyText.debug = {
+	 		cursor : cursor
+			,cursorCycleTimeout : cursorCycleTimeout
+			,cursorHistory : cursorHistory
+			,on : on
+			,off : off
+			,cursorHistoryClear : cursorHistoryClear
+			,cursorCycle : cursorCycle
+	 	}
+		console.log("Loaded GoofyText")	
+		 	
+	 }
+
+	
+})();
