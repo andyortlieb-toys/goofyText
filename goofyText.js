b13d6cd2c0c81f6777562e6898d238f249598b9f
Andy Ortlieb <andy.ortlieb@corvisa.com>
2012-07-11 13:46:26 -0500

    One giant software company sucks at making web browsers

--- goofyText.js
+++ goofyText.js
@@ -1,12 +1,5 @@
 ;(function(){
 
-	// Public declarations
-
-	/** 
-	 * public goofyText
-	 * Namespace for all public goofyText references and methods
-	 */
-	goofyText = {};
 
 	// End public declarations
 
@@ -24,13 +17,32 @@
 
 	; // End private declarations
 
+	/**
+	 * private method eventManager
+	 */
+	function eventManager(obj, eventName){
+		var salvage = obj['on'+eventName];
+
+		obj['on'+eventName] = function(){
+			for (var i=0; i<this.goofyTextListeners[eventName].length; ++i){
+				this.goofyTextListeners[eventName][i].apply(this, arguments);
+			}
+		}
+	}
 
 	/**
 	 * private method on
 	 * enables events, intended for DOM objects.	 
 	 */
 	function on(evtName, callBack){
-		if (this.addEventListener){
+		if (this===document && !this.goofyTextListeners) this.goofyTextListeners = {};
+		if (this===document && !this.goofyTextListeners[evtName]) this.goofyTextListeners[evtName] = eventManager(this, evtName)
+
+		if (this.goofyTextListeners) {
+			if (!this.goofyTextListeners[evtName]) this.goofyTextListeners[evtName] = [];
+			return this.goofyTextListeners[evtName].push(callBack);
+
+		} else if (this.addEventListener){
 			return this.addEventListener(evtName, callBack)
 
 		} else if (this.attachEvent){
@@ -91,6 +103,124 @@
 
 	}
 
+	/**
+	 * private initializeGoofyTextElement
+	 * Converts an element into a goofyText 
+	 */
+	function initializeGoofyTextElement(el){
+		if (el.goofyTextInitialized) return;
+		el.goofyTextInitialized = true;
+
+		console.log("Stub: convert any existing text")
+	}
+
+
+	function handleKeydown (evt){ 
+		console.log('handleKeyDown');
+		if (!cursor) return;
+
+		var evt = evt || window.event
+		var chr = String.fromCharCode(evt.keyCode)
+		console.log("keydown", evt.keyCode)
+
+		switch (evt.keyCode){
+			case 8: // backspace
+				var search = cursor.previousSibling;
+				
+				if (search && search.className === 'chr'){
+					cursor.parentNode.removeChild(search)
+				}
+				
+				// Stupid thing to stop the browser from going back.
+				evt.keyCode = 0;
+
+				goofyText.suppressNextKeypress=true;
+				break;
+
+			case 9: // tab
+				cursor = null;
+
+				goofyText.suppressNextKeypress=true;
+				break;
+
+			case 37: // left
+
+				console.log ("Going left")
+				var search = cursor;
+				while (search !== null){
+					search = search.previousSibling;
+					if (search && search.className === 'chr'){
+						search.click();
+						break;
+					}
+				}
+
+				evt.preventDefault()
+
+				goofyText.suppressNextKeypress=true;
+				break;
+
+			case 39: // right
+
+				console.log ("Going right")
+				var search = cursor;
+				while (search !== null){
+					search = search.nextSibling;
+					if (search && search.className === 'chr'){
+						search.click();
+						break;
+					}
+				}
+
+				goofyText.suppressNextKeypress=true;
+				break;
+
+			case 46: // del
+				var search = cursor.nextSibling;
+				
+				if (search && search.className === 'chr'){
+					cursor.parentNode.removeChild(search)
+				}
+				
+				// Stupid thing to stop the browser from going back.
+				evt.keyCode = 0;								
+
+				goofyText.suppressNextKeypress = true;
+				break;
+
+			default:
+				goofyText.suppressNextKeypress=false;
+
+
+		}
+
+	}
+
+
+
+	// Public declarations
+
+	/** 
+	 * public goofyText
+	 * Namespace for all public goofyText references and methods
+	 */
+	goofyText = {};	
+
+	goofyText.editor= function(){
+		if (arguments.length){
+			for (var i=0; i<arguments.length; ++i){
+				initializeGoofyTextElement(arguments[i]);
+			}
+
+		} else {
+
+			var element = document.createElement("DIV");
+			initializeGoofyTextElement(element);
+			return element;
+
+		}
+	}
+
 
 	/****************************************
 	 ****************************************
@@ -99,6 +229,9 @@
 
 	 ****************************************
 	 ****************************************/
+
+
+	 on.call(document, 'keydown', handleKeydown)
 
 	 // DEBUG STUFF
 	 if (true){
