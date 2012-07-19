8a465816b2ec97d6eec5a259a76b4039e2cff35b
Andy Ortlieb <andy.ortlieb@corvisa.com>
2012-07-19 13:21:22 -0500

    Always relieve the styles when targeting

--- src/goofyText.js
+++ src/goofyText.js
@@ -17,13 +17,13 @@
 				curtop += obj.offsetTop ;
 			}
 		}
-		
+
 		return [curleft,curtop];
 	}
 
 	/**
 	 * private method on
-	 * enables events, intended for DOM objects.	 
+	 * enables events, intended for DOM objects.
 	 */
 	function on(evtName, callBack){
 
@@ -70,11 +70,11 @@
 
 
 	/**
-	 * Creates a char element 
+	 * Creates a char element
 	 */
 	function mkCharNode (character){
 		var chrNode = document.createElement('span')
-		
+
 		if ( character ==='\n' ) {
 
 			// inline-block span prevents lagging newlines.
@@ -98,7 +98,7 @@
 			} else {
 				chrNode.innerText = character;
 			}
-			
+
 		}
 
 		chrNode.goofyTextChr = true;
@@ -123,7 +123,7 @@
 			var cNode = node.childNodes[0];
 
 			if (!cNode) return false;
-			
+
 			if (cNode.nodeType===3){
 				// It's a text el, break it up int chrs
 				for (var i=0; i<cNode.length; ++i){
@@ -219,30 +219,29 @@
 				if (cursor.targetCharNode.forceRight){
 					cursor.targetCharNode.style.borderRightColor = cursor.nextColor;
 				} else {
-					cursor.targetCharNode.style.borderLeftColor = cursor.nextColor;	
+					cursor.targetCharNode.style.borderLeftColor = cursor.nextColor;
 				}
 
 				var xy = findPos(cursor.targetCharNode);
 				// FIXME:
 				//cursor.hijacker.style.left=''+xy[0]+'px';
-				cursor.hijacker.style.top=''+xy[1]+'px';				
-				
+				cursor.hijacker.style.top=''+xy[1]+'px';
+
 			}
 
 			// Clear & reset the timout
 			clearTimeout(cursor.cycleTimeout);
 			cursor.cycleTimeout = setTimeout( cursor.cycle, update?960:480 );
-		}, 
+		},
 		// Method target puts a cursor around a chrNode
 		target: function(chrNode,forceRight){
 			if (cursor.targetCharNode !== chrNode){
-				cursor.relieve(cursor.targetCharNode);
 				cursor.untarget(cursor.targetCharNode);
 			} else if (forceRight === undefined){
 				// It is the same. just switch sides. (unless it's being set)
 				forceRight = !cursor.targetCharNode.forceRight;
-				cursor.relieve(cursor.targetCharNode);
 			}
+			cursor.relieve(cursor.targetCharNode);
 
 			cursor.targetCharNode = chrNode;
 
@@ -256,7 +255,7 @@
 				cursor.targetCharNode.style.borderRightStyle='solid';
 				cursor.targetCharNode.style.borderRightColor='black';
 				cursor.targetCharNode.style.marginRight='-2px';
-				cursor.targetCharNode.forceRight = true;	
+				cursor.targetCharNode.forceRight = true;
 
 			} else {
 				// Put the cursor to the left of chrNode.
@@ -298,7 +297,7 @@
 					node.style.borderLeftColor='';
 					node.style.marginLeft='';
 				}
-			} 
+			}
 		},
 
 		// isReady, whether or not the cursor is ready to handle keys
@@ -306,11 +305,11 @@
 			return cursor.targetCharNode;
 		},
 		putNode: function(node){
-			
+
 			if (cursor.targetCharNode.forceRight){
 				// Put the new node AFTER the targetCharNode, and target the new node.
 				cursor.targetCharNode.parentNode.insertBefore(node, cursor.targetCharNode.nextSibling);
-				
+
 				if (node.nextSibling && node.nextSibling.goofyTextChr){
 					cursor.target(node.nextSibling);
 				} else {
@@ -353,11 +352,11 @@
 
 			var evt = evt || window.event
 			var chr = String.fromCharCode(evt.keyCode)
-			
-			
+
+
 			switch (evt.keyCode){
 				case 8: // backspace
-					
+
 					if (cursor.targetCharNode.forceRight){
 						var removeGuy = cursor.targetCharNode;
 						cursor.target(cursor.targetCharNode.nextSibling||cursor.targetCharNode.previousSibling, !cursor.targetCharNode.nextSibling);
@@ -367,7 +366,7 @@
 							cursor.targetCharNode.parentNode.removeChild(cursor.targetCharNode.previousSibling)
 						}
 					}
-					
+
 					// Stupid thing to stop the browser from going back.
 					evt.keyCode = 0; // The less obvious approach
 					if (evt.preventDefault) evt.preventDefault(); // the more obvious approach
@@ -399,9 +398,9 @@
 
 				case 35: // End
 					var search = cursor.targetCharNode;
-					
+
 					// Find the thing we want to hit... on the same line.
-					while ( 
+					while (
 							search.parentNode && search.nextSibling && search.nextSibling.parentNode
 							&& (search.parentNode === search.nextSibling.parentNode)
 							&& (search.offsetTop === search.nextSibling.offsetTop )
@@ -419,9 +418,9 @@
 
 				case 36: // Home
 					var search = cursor.targetCharNode;
-					
+
 					// Find the thing we want to hit... on the same line.
-					while ( 
+					while (
 							search.parentNode && search.previousSibling && search.previousSibling.parentNode
 							&& (search.parentNode === search.previousSibling.parentNode)
 							&& (search.offsetTop === search.previousSibling.offsetTop )
@@ -472,12 +471,20 @@
 						search = search.nextSibling;
 						if (search && search.goofyTextChr){
 							cursor.target(search, (search===cursor.targetCharNode));
+							console.log("a'")
 							break;
+
 						} else if ( !cursor.targetCharNode.forceRight ){
 							search = null;
+							console.log("b'")
+
 						}
 					}
-					if (!search) cursor.target(cursor.targetCharNode, true);
+
+					if (!search) {
+						cursor.target(cursor.targetCharNode, true);
+						console.log("c'")
+					}
 
 					// Prevent browser scrolling
 					evt.keyCode = 0; // The less obvious approach
@@ -494,13 +501,13 @@
 						cursor.target(cursor.targetCharNode.nextSibling || cursor.targetCharNode.previousSibling);
 						cursor.targetCharNode.parentNode.removeChild(removeGuy)
 					}
-					
+
 
 					break;
 
 				default:
 					// FIXME: Should we scrap this default?
-					
+
 					break;
 			}
 
@@ -524,7 +531,7 @@
 			initializeNode(element);
 			return element;
 
-		}			
+		}
 	}
 
 	// Public declarations:
@@ -550,7 +557,7 @@
 	on.call(cursor.hijacker, 'input', function(){
 		cursor.processHijacker();
 	});
-		
+
 	} else {
 		on.call(cursor.hijacker, 'keydown', function(){
 			setTimeout( cursor.processHijacker, 5);
@@ -570,7 +577,7 @@
 	document.body.appendChild(cursor.hijacker);
 
 	// Debugging info-- cut from builds
-	if (true){ 
+	if (true){
 		// Global
 		goofyTextDebug = {
 			cursor: cursor
